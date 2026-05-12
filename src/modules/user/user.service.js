9c2d74ac518811da559d57aa31a9d8c0de98a725
Thyago Biagio <thybiagio@gmail.com>
2026-05-12 20:21:41 -0300

    atualização user.service

--- src/modules/user/user.service.js
+++ src/modules/user/user.service.js
@@ -6,5 +6,11 @@ export const register = async (data, UserModel) => {
         throw new Error('As senhas não coincidem');
     }
 
+    //Validação 2: senha tem pelo menos 8 caracteres
+    if (password.length < 8) {
+        throw new Error('A senha deve ter pelo menos 8 caracteres');
+    }
+
     return { message: 'Usuário criado com sucesso (ainda sem salvar no banco)' };
 };
+
